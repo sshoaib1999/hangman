0fafeef2f0b9121505894d6a5a28e8a9904268b1
sshoaib1999 <sshoaib1999@hotmail.com>
2023-09-26 16:28:34 +0100

    Add files via upload

---
+++ milestone_3.py
@@ -0,0 +1,21 @@
+import random
+
+words = ['apple', 'banana', 'orange', 'strawberry', 'kiwi'] 
+secret_word = random.choice(words)
+
+def ask_for_input():
+  guess = input("Guess a letter: ")
+  return guess
+
+def check_guess(guess):
+  if guess in secret_word:
+    print(f"Good guess! {guess} is in the word.") 
+  else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
+
+while True:
+  guess = ask_for_input()
+  check_guess(guess)
+  
+  if guess in secret_word:
+    break
